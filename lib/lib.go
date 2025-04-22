749b021483265a6b7c7788f80ea33f7e5be07e86
Maciej Urban <maciej-urban@outlook.com>
2025-04-22 22:17:38 +0200

    refactor: move out stuff from lib

--- lib/lib.go
+++ lib/lib.go
@@ -1,14 +1,11 @@
 package lib
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 type Mode int
@@ -18,119 +15,10 @@ var opts = slog.HandlerOptions{Level: &Level}
 var Logger = slog.New(slog.NewTextHandler(os.Stdout, &opts))
 
 const (
-	// hard copy
-	Cpy Mode = iota
-	// symlinks
-	Dev
-)
-
-type lockfile struct {
-	Version        string   `json:"version"`
-	Mode           Mode     `json:"mode"`
-	Configs        []config `json:"configs"`
-	SkippedConfigs []config `json:"skippedConfigs"`
-}
-
-func (l *lockfile) AppendSkippedConfig(config config) {
-	l.SkippedConfigs = append(l.SkippedConfigs, config)
-}
-
-func NewLockfile() lockfile {
-	return lockfile{
-		Version:        "0.1.0",
-		Configs:        []config{},
-		SkippedConfigs: []config{},
-	}
-}
-
-var DefaultLockfile = NewLockfile()
-
-type config struct {
-	Name         string       `json:"name"`
-	From         string       `json:"from"`
-	To           string       `json:"to"`
-	Requirements requirements `json:"requirements"`
-	InstallInfo  installInfo  `json:"installationInfo"`
-}
-
-type installInfo struct {
-	IsInstalled             bool   `json:"isInstalled"`
-	InstallationTime        string `json:"installationTime"`
-	InstallationInstruction string `json:"installationInstruction"`
-
-	WasUninstalled           bool   `json:"wasUninstalled"`
-	UninstallationTime       string `json:"uninstallationTime"`
-	UinstallationInstruction string `json:"uinstallationInstruction"`
-}
-
-func NewConfig(name, from, to string, reqs *requirements) config {
-	usedReqs := &requirements{}
-	if reqs != nil {
-		usedReqs = reqs
-	}
-	return config{
-		Name:         name,
-		From:         from,
-		To:           to,
-		Requirements: *usedReqs,
-	}
-}
-
-func (c *config) Equal(o *config) bool {
-	return c.Name == o.Name && c.From == o.From && c.To == o.To
-}
-
-func ContainsConfig(configs []config, c config) bool {
-	for _, config := range configs {
-		if config.Equal(&c) {
-			return true
-		}
-	}
-	return false
-}
-
-type InstallationMethod string
-
-const (
-	apt    = "apt"
-	pacman = "pacman"
-	cargo  = "cargo"
-	system = "system"
-	bash   = "bash"
-)
-
-func isValidInstallationMethod(method string) bool {
-	switch method {
-	case apt, pacman, cargo, system, bash:
-		return true
-	default:
-		return false
-	}
-}
-
-// TODO:
-// func uninstallationMethodBasedOnInstallationMethod(instMethod InstallationMethod) UninstallationInstructions
-
-const (
 	INSTALL_PATH_POSTFIX      = "/INSTALL"
-	UNINSTALL_PATH_POSTFIX    = "/UNINSTALL"
+	UNINSTLL_PATH_POSTFIX     = "/UNINSTALL"
 	DEPENDENCIES_PATH_POSTFIX = "/DEPENDENCIES"
 )
-
-type requirements struct {
-	Name         string                     `json:"name"`
-	Install      InstallationInstruction    `json:"installationInstructions"`
-	Uninstall    UninstallationInstructions `json:"uninstallInstructions"`
-	Dependencies []InstallationInstruction  `json:"dependencies"`
-}
-
-type InstallationInstruction struct {
-	Method InstallationMethod `json:"method"`
-	Pkg    string             `json:"pkg"`
-}
-
-// TODO: think what this should include
-type UninstallationInstructions struct{}
 
 func ParseRequirements(path string) (res *requirements, err error) {
 	Logger.Debug("parsing requirements", "path", path)
@@ -162,96 +50,6 @@ func ParseRequirements(path string) (res *requirements, err error) {
 	return res, err
 }
 
-func parseInstallationInstructions(path string) (res *InstallationInstruction, err error) {
-	res = &InstallationInstruction{}
-	file, err := os.Open(path + INSTALL_PATH_POSTFIX)
-	if err != nil {
-		// NOTE: file not existing is not an error in this case (can have config
-		// files without installation instructions obviously)
-		if os.IsNotExist(err) {
-			return res, nil
-		}
-		return nil, err
-	}
-	defer file.Close()
-
-	{
-		txtBytes, err := io.ReadAll(file)
-		if err != nil {
-			return nil, err
-		}
-
-		txt := string(txtBytes)
-		res, err = parseSingleInstallationInstruction(txt)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return res, nil
-}
-
-func parseSingleInstallationInstruction(inst string) (res *InstallationInstruction, err error) {
-	res = &InstallationInstruction{}
-
-	{
-		linesCount := strings.Count(inst, "\n")
-		assert(linesCount <= 1, "think on how to handle multiple installation instructions if we want them in the future")
-	}
-
-	parts := strings.Split(inst, ":")
-	{
-		method := parts[0]
-		errMsg := fmt.Sprintf("must be an implemented, valid installation method, instead got: '%s'", method)
-		assert(isValidInstallationMethod(method), errMsg)
-		res.Method = InstallationMethod(method)
-	}
-
-	{
-		pkg := parts[1]
-		res.Pkg = strings.Trim(pkg, "\n\t")
-	}
-	return res, nil
-}
-
-func parseUinstallationInstructions(path string) (res *UninstallationInstructions, err error) {
-	panic("unimplemented")
-	// return res, err
-}
-
-func parseDependencies(path string) (res []InstallationInstruction, err error) {
-	res = []InstallationInstruction{}
-	file, err := os.Open(path + DEPENDENCIES_PATH_POSTFIX)
-	if err != nil {
-		// NOTE: file not existing is not an error in this case (can have
-		// config files without dependencies obviously)
-		if os.IsNotExist(err) {
-			return res, nil
-		}
-		return nil, err
-	}
-	defer file.Close()
-
-	txtBytes, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	lines := strings.SplitSeq(string(txtBytes), "\n")
-	for line := range lines {
-		if line == "" {
-			continue
-		}
-		instructions, err := parseSingleInstallationInstruction(line)
-		if err != nil {
-			return nil, err
-		}
-		res = append(res, *instructions)
-	}
-
-	return res, err
-}
-
 func ExecuteInstall(info requirements) error {
 	return nil
 }
@@ -273,118 +71,6 @@ func RemoveConfigsFromTarget(configs []config) error {
 		}
 	}
 	return nil
-}
-
-type LockfileDiff struct {
-	AddedConfigs             []config `json:"addedConfigs"`
-	RemovedConfigs           []config `json:"removedConfigs"`
-	NewlySkippedConfigs      []config `json:"newlySkippedConfigs"`
-	PreviouslySkippedConfigs []config `json:"previouslySkippedConfigs"`
-	ModeChanged              bool     `json:"modeChanged"`
-	VersionChanged           bool     `json:"versionChanged"`
-}
-
-// method should be called on an old version of the lockfile
-func (l *lockfile) Diff(newLockfile *lockfile) LockfileDiff {
-	addedConfigs := []config{}
-	removedConfigs := []config{}
-	newlySkippedConfigs := []config{}
-	previouslySkippedConfigs := []config{}
-
-	for _, prevConf := range l.Configs {
-		if !ContainsConfig(newLockfile.Configs, prevConf) {
-			removedConfigs = append(removedConfigs, prevConf)
-		}
-
-		Logger.Debug("DIFFING", "SkippedConfigs", newLockfile.SkippedConfigs, "prevConf", prevConf)
-		if ContainsConfig(newLockfile.SkippedConfigs, prevConf) && !ContainsConfig(l.SkippedConfigs, prevConf) {
-			newlySkippedConfigs = append(newlySkippedConfigs, prevConf)
-		}
-	}
-
-	for _, newConf := range newLockfile.Configs {
-		if !ContainsConfig(l.Configs, newConf) {
-			addedConfigs = append(addedConfigs, newConf)
-		}
-
-	}
-
-	for _, prevSkippedConf := range l.SkippedConfigs {
-		if !ContainsConfig(newLockfile.SkippedConfigs, prevSkippedConf) {
-			previouslySkippedConfigs = append(previouslySkippedConfigs, prevSkippedConf)
-		}
-	}
-
-	return LockfileDiff{
-		AddedConfigs:             addedConfigs,
-		RemovedConfigs:           removedConfigs,
-		NewlySkippedConfigs:      newlySkippedConfigs,
-		PreviouslySkippedConfigs: previouslySkippedConfigs,
-		ModeChanged:              l.Mode != newLockfile.Mode,
-		VersionChanged:           l.Version != newLockfile.Version,
-	}
-}
-
-func ReadOrCreateLockfile(path string) (*lockfile, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			f, err := os.Create(path)
-			if err != nil {
-				return nil, err
-			}
-			defer f.Close()
-			defaultLockfileBytes, _ := DefaultLockfile.Marshal()
-			written, err := f.Write(defaultLockfileBytes)
-			if err != nil {
-				return nil, err
-			}
-			assert(written == len(defaultLockfileBytes), "must write what is given")
-			return &DefaultLockfile, nil
-		}
-		return nil, err
-	}
-	defer file.Close()
-	txt, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-	return parseLockfile(txt)
-}
-
-func (l *lockfile) Save(path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	toWrite, err := l.Marshal()
-	if err != nil {
-		return err
-	}
-	written, err := file.Write(toWrite)
-	if err != nil {
-		return err
-	}
-	assert(written == len(toWrite), "must write what is given")
-	return nil
-}
-
-func (l *lockfile) AddConfig(config config) {
-	l.Configs = append(l.Configs, config)
-}
-
-func parseLockfile(txt []byte) (*lockfile, error) {
-	lockfile := lockfile{}
-	err := json.Unmarshal(txt, &lockfile)
-	if err != nil {
-		return nil, err
-	}
-	return &lockfile, nil
-}
-
-func (l *lockfile) Marshal() ([]byte, error) {
-	return json.Marshal(*l)
 }
 
 func Symlink(from, to string) error {
