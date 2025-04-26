344389b6e4fb96709c540bb9930c94ee562e9e2d
Maciej Urban <maciej-urban@outlook.com>
2025-04-26 19:00:35 +0200

    refactor: move instructions to another file

--- lib/lib.go
+++ lib/lib.go
@@ -121,38 +121,6 @@ func install(inst installInstruction) (cmd string, err error) {
 	return cmd, err
 }
 
-func installWithCargoCmd(pkg string) (string, error) {
-	cmd := "cargo install " + pkg
-	return cmd, nil
-}
-
-func installWithCargoBinstallCmd(pkg string) (string, error) {
-	cmd := "cargo-binstall " + pkg
-	return cmd, nil
-}
-
-func installWithPacmanCmd(pkg string) (string, error) {
-	cmd := "sudo pacman -Syy " + pkg
-	return cmd, nil
-}
-
-func installWithAurCmd(pkg string) (string, error) {
-	// TODO: detect the aur manager used and use that instead of using yay by default
-	cmd := "yay -S --sudoloop " + pkg
-	return cmd, nil
-}
-
-func installWithSystemCmd(pkg string) (string, error) {
-	cmd, err := genSystemInstallCmd(pkg)
-	return cmd, err
-}
-
-func genSystemInstallCmd(pkg string) (string, error) {
-	panic("not implemented yet")
-	cmd := pkg
-	return cmd, nil
-}
-
 func execute(cmd string) error {
 	splitCmd := strings.Split(cmd, " ")
 	{
