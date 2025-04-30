25ad65208392dcb15e12758f1e0132a8e96328c3
Maciej Urban <maciej-urban@outlook.com>
2025-04-30 21:41:17 +0200

    feat: install command

--- lib/actions.go
+++ lib/actions.go
@@ -46,52 +46,34 @@ func Remove(c *configuration.Configuration, configs []config) error {
 	return nil
 }
 
-func FindPkgsToInstall(c *configuration.Configuration, ld *lockfileDiff) ([]installInstruction, error) {
-	instructions := []installInstruction{}
-	return instructions, nil
-}
-
-func Install(c *configuration.Configuration, l *lockfile, instructions []installInstruction) error {
-	for _, inst := range instructions {
-		cmd, err := install(inst)
-		if err != nil {
-			return err
+func Install(lock *lockfile) map[string]installInfo {
+	forUpdate := make(map[string]installInfo)
+	for _, cfg := range lock.Configs {
+		if cfg.InstallInfo.IsInstalled {
+			Logger.Debug("skipping installation of already installed packages for config", "cfgName", cfg.Name)
+			continue
 		}
-	}
-	return nil
-}
 
-// func Install(cfg config) (res *installInfo, err error) {
-// 	res, err = &cfg.InstallInfo, nil
-//
-// 	if cfg.Requirements.Install == nil {
-// 		Logger.Debug("not installing the pkg, because there was no INSTALL instructions", "pkg", cfg.Name)
-// 		return res, nil
-// 	}
-//
-// 	// first install the dependencies if any
-// 	{
-// 		err = installDependencies(cfg.Requirements.Dependencies)
-// 		if err != nil {
-// 			return res, err
-// 		}
-// 		res.DependenciesInstalled = true
-// 	}
-//
-// 	{
-// 		cmd, err := install(*cfg.Requirements.Install)
-// 		if err != nil {
-// 			return res, err
-// 		}
-// 		res.InstallInstruction = cmd
-// 	}
-//
-// 	// info handling
-// 	{
-// 		res.InstallTime = now()
-// 		res.IsInstalled = true
-// 		res.WasUninstalled = false
-// 		res.UninstallTime = ""
-// 	}
-// 	return res, err
-// }
+		info := installInfo{}
+		err := installDependencies(cfg.Requirements.Dependencies)
+		if err != nil {
+			Logger.Debug("something went wrong while installing dependencies, trying to continue", "cfgName", cfg.Name, "err", err)
+			continue
+		}
+		info.DependenciesInstalled = true
+
+		cmd, err := install(*cfg.Requirements.Install)
+		if err != nil {
+			Logger.Debug("something went wrong while installing dependencies, trying to continue", "cfgName", cfg.Name, "err", err)
+			continue
+		}
+		info.InstallTime = now()
+		info.InstallInstruction = cmd
+		info.IsInstalled = true
+		info.WasUninstalled = false
+		info.UninstallTime = ""
+		info.UninstallInstruction = ""
+		forUpdate[cfg.Name] = info
+	}
+	return forUpdate
+}
